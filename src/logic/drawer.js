722b49c7a38310daf4a73f3e6aa48c6af0accf90
Aleksey Ivanov <aleksey.ivanov@arcadia.spb.ru>
2020-01-27 10:52:22 +0300

    add squares numeration (not so beauty)

--- src/logic/drawer.js
+++ src/logic/drawer.js
@@ -10,10 +10,15 @@ export class CanvasDrawer {
         canvas.height = 800;
         canvas.width = 600;
 
+        this.fontSize = this.squareSize / 2;
+        this.numerationPadding = this.fontSize * 2;
+
         this.colorScheme = colorScheme;
 
         if (canvas.getContext) {
             this._context = canvas.getContext('2d');
+            this._context.font = `${this.fontSize}px serif`;
+            this._context.textAlign = 'center';
         } else {
             throw new Error('Not canvas!');
         }
@@ -22,9 +27,11 @@ export class CanvasDrawer {
     draw(field) {
         this.clear();
         field.squares.forEach((row, x) => {
+            this._drawXNumeration(x);
             row.forEach((square, y) => {
                 this._context.beginPath();
                 this._drawSquare(x, y);
+                this._drawYNumeration(y);
                 if (square.figure) {
                     this._drawFigure(square.figure, x, y);
                 }
@@ -39,7 +46,7 @@ export class CanvasDrawer {
 
     _drawSquare(x, y) {
         this._setStrokeColor('black');
-        this._context.strokeRect(x * this.squareSize, y * this.squareSize, this.squareSize, this.squareSize);
+        this._context.strokeRect(this._nextSquareCoordinate(x), this._nextSquareCoordinate(y), this.squareSize, this.squareSize);
     }
 
     _drawFigure(figure, x, y) {
@@ -54,8 +61,22 @@ export class CanvasDrawer {
         }
     }
 
+    _drawXNumeration(x) {
+        this._setFillColor('blue');
+        this._context.fillText(x, this._centerOfSquare(x), this.fontSize * 1.5);
+    }
+
+    _drawYNumeration(y) {
+        this._setFillColor('orange');
+        this._context.fillText(y, this.fontSize, this._centerOfSquare(y) + 5);
+    }
+
+    _nextSquareCoordinate(coord) {
+        return coord * this.squareSize + this.numerationPadding;
+    }
+
     _centerOfSquare(coord) {
-        return this.squareSize * (coord + 0.5);
+        return this.squareSize * (coord + 0.5) + this.numerationPadding;
     }
 
     _setFigureColor(figure) {
